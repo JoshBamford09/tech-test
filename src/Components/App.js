23d857461ab311b5ca271e056d6fda0183515d7f
Josh Bamford <joshbamford09@gmail.com>
2023-02-02 21:33:56 +0000

    imported image locally

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../Styles/app.css';
 import SearchComponent from "../Components/Search"
+import NasaLogo from "../Images/nasa.jpg"
 
 const App = () => {
   return (
     <div className='app'>
-      <img calssName="" src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg" alt='Nasa Logo' />
+      <img calssName="" src={NasaLogo} alt='Nasa Logo' />
       <SearchComponent />
     </div>
   );
